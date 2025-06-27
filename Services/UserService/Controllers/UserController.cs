f169daf1642f905c35aa192dc247c3b912b8f1c8
Vikram <vikram@joviantechnologies.io>
2025-06-27 16:31:16 +0530

    Pushing changes for UserServices (with FakeUserService used for in-memory data)

---
+++ Services/UserService/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
+using UserService.Services;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly FakeUserService _userService;
+
+        public UserController(FakeUserService fakeUserService)
+        {
+            _userService = fakeUserService;            
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers([FromQuery] int page = 1, [FromQuery] int pageSize = 5) 
+        { 
+            var users = _userService.GetPagedUsers(page, pageSize);
+
+            var total = _userService.TotalCount;
+
+            return Ok(new {
+                Page = page,
+                PageSize = pageSize,
+                Total = total,
+                Users = users
+            });
+        }
+    }
+}
---
+++ Services/UserService/Services/FakeUserService.cs
@@ -0,0 +1,29 @@
+﻿using UserService.Models;
+
+namespace UserService.Services
+{
+    public class FakeUserService
+    {
+        private List<User> _users;
+        public FakeUserService()
+        {
+            _users = Enumerable.Range(1, 30).Select(i => new User
+            {
+                Id = i,
+                FirstName = "First" + i.ToString(),
+                LastName = "Last" + i.ToString(),
+                Email = "user" + i.ToString() + "@demo.com"
+            }).ToList();
+        }
+
+        public IEnumerable<User> GetPagedUsers(int page, int pageSize) 
+        {
+            IEnumerable<User> pagedUsers = new List<User>();
+            pagedUsers = _users.Skip(pageSize * (page - 1)).Take(pageSize);
+
+            return pagedUsers;
+        }
+
+        public int TotalCount => _users.Count;
+    }
+}
